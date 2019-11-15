c520e4583698b367fa49c85ef84e892cf8f43070
Swar Gujrania <swar.gujrania@gmail.com>
2019-11-15 00:30:10 -0500

    buckets

--- Buckets/main.js
+++ Buckets/main.js
@@ -12,6 +12,9 @@ const incomeBrackets = [
     "$75000 or more"
 ];
 
+const chartWidth = 1000;
+const chartHeight = 1000;
+
 const colorMapping = {
     "$1 to $4,999 or loss": "red",
     "$5000 to $14999": "orange",
@@ -29,18 +32,60 @@ d3.csv('../earnings.csv').then(data => {
         .object(data);
 
     var dataForBuckets = dataForVisualMapping(nestedData);
-
     console.log(dataForBuckets);
 
-    var svg = d3.select('#main').select('svg');
+    svg = d3.select('#main').select('svg');
+
+    //choose data for bucket size
+
+    //withDisability
+    var lineCoordinates = getLineCoordinates(dataForBuckets['WithDisability']);
+    console.log(lineCoordinates);
+
+    buckets = svg.selectAll('line')
+        .data(dataForBuckets['WithDisability'])
+        .enter()
+        .append('line')
+        .attr('x1', function (d) { var coordinates = lineCoordinates[d['income_bracket']]; return coordinates.x1})
+        .attr('y1', function (d) { var coordinates = lineCoordinates[d['income_bracket']]; return coordinates.y1})
+        .attr('x2', function (d) { var coordinates = lineCoordinates[d['income_bracket']]; return coordinates.x2})
+        .attr('y2', function (d) { var coordinates = lineCoordinates[d['income_bracket']]; return coordinates.y2})
+        .attr('stroke', 'black')
+        .attr('stroke-width', 2)
+        .exit();
+
 
 });
+
+function getLineCoordinates(dataBucket) {
+    coordinates = {};
+    var totalVal = d3.sum(dataBucket, function (d) { return +d['value'] });
+
+    //computing x
+
+    prevX = 0;
+
+    for (var i in incomeBrackets) {
+        var s = incomeBrackets[i];
+        coordinates[s] = {};
+        coordinates[s]['x1'] = prevX + 20;
+        coordinates[s]['x2'] = prevX + 20 + (dataBucket.find(d => d['income_bracket'] == s)['value'] / totalVal) * chartWidth;
+        coordinates[s]['y1'] = 100;
+        coordinates[s]['y2'] = 100;
+
+        prevX = coordinates[s]['x2']
+    }
+
+    //todo: computing y
+
+    return coordinates
+}
 
 
 function dataForVisualMapping(nestedData) {
 
     delete nestedData['Total Civilian Noninstitutionalized Population'];
-    
+
     var BracketData = [];
 
     for (let status in nestedData) {
@@ -58,20 +103,25 @@ function dataForVisualMapping(nestedData) {
         }
     }
 
-    WithDisabilityBracketData = BracketData.filter(e => e['status'] == 'With a Disability');
-    NoDisabilityBracketData = BracketData.filter(e => e['status'] == 'No Disability');
+    WithDisabilityBracketData = [];
+    incomeBrackets.forEach(i => {
+        WithDisabilityBracketData.push({ 'income_bracket': i, 'value': BracketData.find(d => d['status'] == 'With a Disability' && d['income_bracket'] == i)['value'] })
+    })
 
-    AllDisabilityBracketData = []
+    NoDisabilityBracketData = [];
+    incomeBrackets.forEach(i => {
+        NoDisabilityBracketData.push({ 'income_bracket': i, 'value': BracketData.find(d => d['status'] == 'No Disability' && d['income_bracket'] == i)['value'] })
+    })
 
-    incomeBrackets.forEach(i => AllDisabilityBracketData.push({
-        'income_bracket': i,
-        'value': WithDisabilityBracketData.find(w => w['income_bracket'] == i)['value'] + NoDisabilityBracketData.find(w => w['income_bracket'] == i)['value']
-    }))
+    AllDisabilityBracketData = {}
+    incomeBrackets.forEach(i => {
+        AllDisabilityBracketData[i.toString()] = WithDisabilityBracketData.find(d => d['income_bracket'] == i)['value'] + NoDisabilityBracketData.find(d => d['income_bracket'] == i)['value'];
+    })
 
     return {
         'WithDisability': WithDisabilityBracketData,
         'NoDisability': NoDisabilityBracketData,
-        'All': AllDisabilityBracketData,
+        'AllDisability': AllDisabilityBracketData,
     }
 
 }
