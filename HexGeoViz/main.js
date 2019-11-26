a4ba5423956c5b809dc4c39e0c7836d5eb911f03
Swar Gujrania <swar.gujrania@gmail.com>
2019-11-26 17:18:14 -0500

    accessing individual coordinates

--- HexGeoViz/main.js
+++ HexGeoViz/main.js
@@ -8,15 +8,15 @@ d3.json("topo2018.json").then(tilegram => {
     console.log(tilegram);
 
     tiles = topojson.feature(tilegram, tilegram.objects.tiles)
-    var transform = d3.geoTransform({
+    transform = d3.geoTransform({
         point: function (x, y) {
             this.stream.point(x, -y)
         }
     });
 
 
-
-    var path = d3.geoPath().projection(transform);
+    // old code
+    Dpath = d3.geoPath().projection(transform);
 
     const svg = d3.select('#main').append("svg")
         .attr('width', WIDTH)
@@ -30,13 +30,13 @@ d3.json("topo2018.json").then(tilegram => {
         .enter()
         .append('path')
         .attr('class', 'tiles')
-        .attr('d', path)
+        .attr('d', Dpath)
         .attr('fill', '#fff')
-        .attr('stroke', '#f1856f');
-
+        .attr('stroke', '#f1856f')
+        .attr('id', function (d) { return d.properties.name + '_path' });
     d3.selectAll('.tiles')
         .on('mouseover', function (d) {
-            $(this).attr('fill', 'red');
+            $(this).attr('fill', 'yellow');
         })
         .on('mouseout', function (d) {
             $(this).attr('fill', '#fff');
@@ -45,35 +45,67 @@ d3.json("topo2018.json").then(tilegram => {
 
         });
 
-    // Build list of state codes
-    var stateCodes = []
-    tilegram.objects.tiles.geometries.forEach(function (geometry) {
-        if (stateCodes.indexOf(geometry.properties.name) === -1) {
-            stateCodes.push(geometry.properties.name)
-        }
-    })
+    // // Build list of state codes
+    // var stateCodes = []
+    // tilegram.objects.tiles.geometries.forEach(function (geometry) {
+    //     if (stateCodes.indexOf(geometry.properties.name) === -1) {
+    //         stateCodes.push(geometry.properties.name)
+    //     }
+    // })
 
-    // Build merged geometry for each state
-    var stateBorders = stateCodes.map(function (code) {
-        return topojson.merge(
-            tilegram,
-            tilegram.objects.tiles.geometries.filter(function (geometry) {
-                return geometry.properties.name === code
-            })
-        )
-    })
+    // // Build merged geometry for each state
+    // var stateBorders = stateCodes.map(function (code) {
+    //     return topojson.merge(
+    //         tilegram,
+    //         tilegram.objects.tiles.geometries.filter(function (geometry) {
+    //             return geometry.properties.name === code
+    //         })
+    //     )
+    // })
 
-    // Draw path
-    g.selectAll('path.border')
-        .data(stateBorders)
-        .enter().append('path')
-        .attr('d', path)
-        .attr('class', 'border')
-        .attr('fill', 'none')
-        .attr('stroke', 'black')
-        .attr('stroke-width', 4)
+    // // Draw path
+    // g.selectAll('path.border')
+    //     .data(stateBorders)
+    //     .enter().append('path')
+    //     .attr('d', path)
+    //     .attr('class', 'border')
+    //     .attr('fill', 'none')
+    //     .attr('stroke', 'black')
+    //     .attr('stroke-width', 4)
 
 
+    //new code
+    //get coordinates individually
+    Coordinates = tiles.features.map(t => t.geometry).map(u => u.coordinates).flat();
+
+    g.selectAll('polyline')
+        .data(Coordinates)
+        .enter()
+        .append('polyline')
+        .attr('fill', '#fff')
+        .attr('opacity', '0.1')
+        .attr('stroke', 'none')
+        .attr('points', d => {
+            var x = "";
+            //console.log(d);
+            for (j = 0; j < 7; j++) {
+                x += d[0][j][0] + ',' + (d[0][j][1] * (-1)) + ',';
+            }
+
+
+            return x.substring(0, x.length - 1);
+        })
+        .on('mouseover', function (d) {
+            $(this).attr('fill', 'red');
+            $(this).attr('opacity', '1');
+        })
+        .on('mouseout', function (d) {
+            $(this).attr('fill', '#fff');
+            $(this).attr('opacity', '0.1');
+        })
+        .on('mousemove', function (d) {
+
+        });
 
 })
 
