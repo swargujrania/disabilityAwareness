0450c551ee6d02ea1ea5616223552a9fcce9b580
Swar Gujrania <swar.gujrania@gmail.com>
2019-11-26 20:06:46 -0500

    statewise hexagons

--- HexGeoViz/main.js
+++ HexGeoViz/main.js
@@ -2,7 +2,8 @@
 var WIDTH = 2500
 var HEIGHT = 2000
 
-
+colorPalette = ['#ffd600', '#c9e402', '#b6ff64', '#63c964', '#6fffbf', '#67b1a0', '#00caba', '#06efff', '#008eaa', '#2284dd'];
+$('#stateName').css('left', WIDTH/2);
 
 d3.json("topo2018.json").then(tilegram => {
     console.log(tilegram);
@@ -25,25 +26,25 @@ d3.json("topo2018.json").then(tilegram => {
     var g = svg.append('g')
         .attr('transform', 'translate(-400,' + 1300 + ')')
 
-    g.selectAll('path')
-        .data(tiles.features)
-        .enter()
-        .append('path')
-        .attr('class', 'tiles')
-        .attr('d', Dpath)
-        .attr('fill', '#fff')
-        .attr('stroke', '#f1856f')
-        .attr('id', function (d) { return d.properties.name + '_path' });
-    d3.selectAll('.tiles')
-        .on('mouseover', function (d) {
-            $(this).attr('fill', 'yellow');
-        })
-        .on('mouseout', function (d) {
-            $(this).attr('fill', '#fff');
-        })
-        .on('mousemove', function (d) {
+    // g.selectAll('path')
+    //     .data(tiles.features)
+    //     .enter()
+    //     .append('path')
+    //     .attr('class', 'tiles')
+    //     .attr('d', Dpath)
+    //     .attr('fill', '#fff')
+    //     .attr('stroke', '#f1856f')
+    //     .attr('id', function (d) { return d.properties.name + '_path' });
+    // d3.selectAll('.tiles')
+    //     .on('mouseover', function (d) {
+    //         $(this).attr('fill', 'yellow');
+    //     })
+    //     .on('mouseout', function (d) {
+    //         $(this).attr('fill', '#fff');
+    //     })
+    //     .on('mousemove', function (d) {
 
-        });
+    //     });
 
     // // Build list of state codes
     // var stateCodes = []
@@ -76,38 +77,233 @@ d3.json("topo2018.json").then(tilegram => {
 
     //new code
     //get coordinates individually
+    
     Coordinates = tiles.features.map(t => t.geometry).map(u => u.coordinates).flat();
 
+    hexUnitArray = [];
+
+    tiles.features.forEach(element => {
+        var hexCoordinateArray = element.geometry.coordinates;
+
+        hexCoordinateArray.forEach((hexCoordinate, index) => {
+
+            var hexUnit = {
+                'stateId': element.id,
+                'elementId': '',
+                'state': element.properties.name,
+                'points': ''
+            }
+            var point = '';
+
+            for (i = 0; i < hexCoordinate[0].length; i++) {
+                point += hexCoordinate[0][i][0] + ',' + (hexCoordinate[0][i][1] * (-1)) + ','
+            }
+
+            hexUnit.points = point.substring(0, point.length - 1);
+            hexUnit.elementId = index;
+
+            hexUnitArray.push(hexUnit);
+
+        })
+
+    });
+
     g.selectAll('polyline')
-        .data(Coordinates)
+        .data(hexUnitArray)
         .enter()
         .append('polyline')
         .attr('fill', '#fff')
-        .attr('opacity', '0.1')
-        .attr('stroke', 'none')
+        .attr('stroke', d => { return getColorByState(d.state) })
+        .attr('data-state', d => { return d.state; })
         .attr('points', d => {
-            var x = "";
-            //console.log(d);
-            for (j = 0; j < 7; j++) {
-                x += d[0][j][0] + ',' + (d[0][j][1] * (-1)) + ',';
-            }
-
-
-            return x.substring(0, x.length - 1);
+            return d.points;
         })
         .on('mouseover', function (d) {
-            $(this).attr('fill', 'red');
-            $(this).attr('opacity', '1');
+            $(this).attr('fill', getColorByState(d.state));
+            $('#stateName').text("STATE: " + d.state);
         })
         .on('mouseout', function (d) {
             $(this).attr('fill', '#fff');
-            $(this).attr('opacity', '0.1');
+            $('#stateName').text("STATE");
+
         })
         .on('mousemove', function (d) {
 
         });
 
 })
+
+function getColorByState(stateName) {
+
+    var stt = stateName[0] + stateName.substr(1).toLowerCase();
+
+    switch (stt) {
+        case 'Wyoming':
+            return colorPalette[1]
+            break;
+        case 'Wisconsin':
+            return colorPalette[1]
+            break;
+        case 'West Virginia':
+            return colorPalette[1]
+            break;
+        case 'Washington':
+            return colorPalette[1]
+            break;
+        case 'Virginia':
+            return colorPalette[1]
+            break;
+        case 'Virgin Islands':
+            return colorPalette[2]
+            break;
+        case 'Vermont':
+            return colorPalette[2]
+            break;
+        case 'Utah':
+            return colorPalette[2]
+            break;
+        case 'Texas':
+            return colorPalette[2]
+            break;
+        case 'South Dakota':
+            return colorPalette[2]
+            break;
+        case 'Tennessee':
+            return colorPalette[3]
+            break;
+        case 'South Carolina':
+            return colorPalette[3]
+            break;
+        case 'Rhode Island':
+            return colorPalette[3]
+            break;
+        case 'Pennsylvania':
+            return colorPalette[3]
+            break;
+        case 'Oregon':
+            return colorPalette[3]
+            break;
+        case 'Oklahoma':
+            return colorPalette[4]
+            break;
+        case 'Ohio':
+            return colorPalette[4]
+            break;
+        case 'North Dakota':
+            return colorPalette[4]
+            break;
+        case 'North Carolina':
+            return colorPalette[4]
+            break;
+        case "New York":
+            return colorPalette[4]
+            break;
+        case 'New Mexico':
+            return colorPalette[5]
+            break;
+        case 'New Jersey':
+            return colorPalette[5]
+            break;
+        case 'New Hampshire':
+            return colorPalette[5]
+            break;
+        case 'Nevada':
+            return colorPalette[5]
+            break;
+        case 'Nebraska':
+            return colorPalette[5]
+            break;
+        case 'Montana':
+            return colorPalette[6]
+            break;
+        case 'Missouri':
+            return colorPalette[6]
+            break;
+        case 'Mississippi':
+            return colorPalette[6]
+            break;
+        case 'Minnesota':
+            return colorPalette[6]
+            break;
+        case 'Michigan':
+            return colorPalette[6]
+            break;
+        case 'Massachusetts':
+            return colorPalette[7]
+            break;
+        case 'Maryland':
+            return colorPalette[7]
+            break;
+        case 'Maine':
+            return colorPalette[7]
+            break;
+        case 'Louisiana':
+            return colorPalette[7]
+            break;
+        case 'Kentucky':
+            return colorPalette[7]
+            break;
+        case 'Kansas':
+            return colorPalette[8]
+            break;
+        case 'Iowa':
+            return colorPalette[8]
+            break;
+        case 'Indiana':
+            return colorPalette[8]
+            break;
+        case 'Illinois':
+            return colorPalette[8]
+            break;
+        case 'Idaho':
+            return colorPalette[8]
+            break;
+        case 'Hawaii':
+            return colorPalette[9]
+            break;
+        case 'Guam':
+            return colorPalette[9]
+            break;
+        case 'Georgia':
+            return colorPalette[9]
+            break;
+        case 'Florida':
+            return colorPalette[9]
+            break;
+        case 'District Of Columbia':
+            return colorPalette[9]
+            break;
+        case 'Delaware':
+            return colorPalette[0]
+            break;
+        case 'Connecticut':
+            return colorPalette[0]
+            break;
+        case 'Colorado':
+            return colorPalette[0]
+            break;
+        case 'California':
+            return colorPalette[0]
+            break;
+        case 'Arkansas':
+            return colorPalette[0]
+            break;
+        case 'Arizona':
+            return colorPalette[0]
+            break;
+        case 'Alaska':
+            return colorPalette[0]
+            break;
+        case 'Alabama':
+            return colorPalette[0]
+            break;
+        default:
+            return colorPalette[0]
+            break;
+
+    }
+
+}
 
 
 
