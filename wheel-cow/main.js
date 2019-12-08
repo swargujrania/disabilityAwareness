197c172794dbf090fff1bdc8857c14542dc86cb1
tsawant7 <48068303+tsawant7@users.noreply.github.com>
2019-12-07 20:38:26 -0500

    Added text labels to class of workers' wheel vis

--- wheel-cow/main.js
+++ wheel-cow/main.js
@@ -11,6 +11,7 @@ var totalNumber = [];
 
 start = () => {
 
+    
   var svg = d3.select('#vis-container svg')
   var svgWidth = $('#vis-container .vis svg').width();
   var svgHeight = $('#vis-container .vis svg').height();
@@ -21,17 +22,17 @@ start = () => {
 
   wheel.append('text')
     .attr('id', 'stateName')
-    .attr('transform', `translate(${ svgWidth / 2 - 20 }, ${ svgHeight / 2 - 50 })`)
+    .attr('transform', `translate(${ svgWidth / 2 - 30}, ${ svgHeight / 2 + 50})`)
     .text('');
 
     wheel.append('text')
     .attr('id', 'stateTotal')
-    .attr('transform', `translate(${ svgWidth / 2 - 20 }, ${ svgHeight / 2 - 30 })`)
+    .attr('transform', `translate(${ svgWidth / 2 - 30}, ${ svgHeight / 2 + 70})`)
     .text('');
 
   var hub_r = 100;
   var hub_cx = svgWidth / 2;
-  var hub_cy = svgHeight / 2 + margin.top / 2 - 100;
+  var hub_cy = svgHeight / 2 + margin.top / 2 +10;
 
   var hub = wheel.append('circle')
     .attr('class', 'hub')
@@ -204,18 +205,128 @@ start = () => {
         highlightState(d.state);
       })
 
-    var outerCircle = wheel.append('circle')
-      .attr('class', 'outerCircle')
-      .attr('r', 4 * hub_r)
-      .attr('cx', hub_cx)
-      .attr('cy', hub_cy)
-      .attr('fill', 'none')
-      .attr('stroke', '#ccc');
+    // var outerCircle = wheel.append('circle')
+    //   .attr('class', 'outerCircle')
+    //   .attr('r', 4 * hub_r)
+    //   .attr('cx', hub_cx)
+    //   .attr('cy', hub_cy)
+    //   .attr('fill', 'none')
+    //   .attr('stroke', '#ccc');
 
     var start_x = hub_cx - 350;
     var start_y = hub_cy;
     var end_x = hub_cx + 350;
     var end_y = hub_cy;
+
+    labels1 = ["Unpaid family" ,"Self-employed in own","Federal government","State government","Local government","Private not-for-profit","Self-employed in  ","Employee of private","Private for-profit"];
+      labels2 = ["workers", " not incorporated ","workers","workers","workers","wage and salary","own incorporated","company workers","wage and salary" ];
+      labels3 = ["","business workers","","","","workers","business workers","","workers"];
+      var outerCircleRadius1 = 4.1 *hub_r;
+      var outerCircleRadius2 = 3.9 * hub_r;
+      var outerCircleRadius3 = 3.7 * hub_r;
+     
+   function polarToCartesian(centerX, centerY, radius, angleInRadians) {
+      //var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+      return {
+          x: centerX + (radius * Math.cos(angleInRadians)),
+          y: centerY + (radius * Math.sin(angleInRadians))
+      };
+    }
+
+    function describeArc(x, y, radius, startAngle, endAngle){
+    var start = polarToCartesian(x, y, radius, endAngle + Math.PI);
+    var end = polarToCartesian(x, y, radius, startAngle + Math.PI);
+    var arcSweep = endAngle - startAngle <= 180 ? "0" : "1";
+    var d = [
+        "M", start.x, start.y, 
+        "A", radius, radius, 0, 1, 1, end.x, end.y
+    ].join(" ");
+    return d;       
+    }
+
+
+    var arcs1 = svg.selectAll(".arcs")
+        .data(buckets)
+        .enter()
+        .append("path")
+        .attr("fill","none")
+        .attr("id", function(d,i){return "s"+i;})
+        .attr("d",function(d,i) {
+            return describeArc(hub_cx, hub_cy, outerCircleRadius1, d.theta1, d.theta2);
+        } )
+        .style("stroke", "#AAAAAA")
+        .attr("stroke-opacity", 0)
+        .style("stroke-dasharray", "5,5")
+  var arcs2 = svg.selectAll(".arcs")
+        .data(buckets)
+        .enter()
+        .append("path")
+        .attr("fill","none")
+        .attr("id", function(d,i){return "t"+i;})
+        //.attr("id","huhue")
+        // .attr("d", function(d,i) {
+        //     return describeArc(d.x, d.y, d.r, 160, -160)
+        // } );
+        .attr("d",function(d,i) {
+            return describeArc(hub_cx, hub_cy, outerCircleRadius2, d.theta1, d.theta2);
+        } )//"M"+start_x+","+start_y+", A"+500+","+500+" 0 0,1 "+end_x+","+end_y)
+        .style("stroke", "#AAAAAA")
+        .attr("stroke-opacity", 0)
+        .style("stroke-dasharray", "5,5");
+
+    var arcs3 = svg.selectAll(".arcs")
+        .data(buckets)
+        .enter()
+        .append("path")
+        .attr("fill","none")
+        .attr("id", function(d,i){return "u"+i;})
+        //.attr("id","huhue")
+        // .attr("d", function(d,i) {
+        //     return describeArc(d.x, d.y, d.r, 160, -160)
+        // } );
+        .attr("d",function(d,i) {
+            return describeArc(hub_cx, hub_cy, outerCircleRadius3, d.theta1, d.theta2);
+        } )//"M"+start_x+","+start_y+", A"+500+","+500+" 0 0,1 "+end_x+","+end_y)
+        .style("stroke", "#AAAAAA")
+        .attr("stroke-opacity", 0)
+        .style("stroke-dasharray", "5,5");
+
+    var textArc1 = svg.selectAll(".industryLabels")
+      .data(labels1)
+      .enter()
+      .append("text")
+      .style("text-anchor","middle")
+      .append("textPath")        //append a textPath to the text element
+      .attr("xlink:href",function(d,i){
+        return "#s"+i;
+      })
+      .attr("startOffset",function(d,i){return "50%";}) //place the text halfway on the arc
+      .text(function(d,i){return d;});
+    
+    var textArc2 = svg.selectAll(".industryLabels")
+      .data(labels2)
+      .enter()
+      .append("text")
+      .style("text-anchor","middle")
+      .append("textPath")        //append a textPath to the text element
+      .attr("xlink:href",function(d,i){
+        return "#t"+i;
+      })
+      .attr("startOffset",function(d,i){return "50%";}) //place the text halfway on the arc
+      .text(function(d,i){return d;});
+
+    var textArc3 = svg.selectAll(".industryLabels")
+      .data(labels3)
+      .enter()
+      .append("text")
+      .style("text-anchor","middle")
+      .append("textPath")        //append a textPath to the text element
+      .attr("xlink:href",function(d,i){
+        return "#u"+i;
+      })
+      .attr("startOffset",function(d,i){return "50%";}) //place the text halfway on the arc
+      .text(function(d,i){return d;});
+
 
   });
 }
