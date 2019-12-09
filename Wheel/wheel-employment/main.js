026ff49ffe81ff992d7ae9198b33f84b25e11de9
Swar Gujrania <swar.gujrania@gmail.com>
2019-12-08 21:33:26 -0500

    wheelcolor changed

--- Wheel/wheel-employment/main.js
+++ Wheel/wheel-employment/main.js
@@ -155,7 +155,7 @@ start = () => {
         .append("text")
         .style("text-anchor", "middle")
         .style("font-size", "10px")
-        .style("fill", "rgb(255,0,0, 0.7)")
+        .style("fill", "grey")
         .append("textPath")        //append a textPath to the text element
         .attr("xlink:href", function (d, i) {
           return "#v" + i;
@@ -328,8 +328,8 @@ function drawWheel(svgWidth, svgHeight, spokesData) {
 
 function highlightState(state) {
 
-  $('".dis_unit[data-state=\'' + state + '\']"').attr('fill', 'rgba(0,134,173, 1)');
-  $('".reg_unit[data-state=\'' + state + '\']"').attr('fill', 'rgba(0,134,173, 0.4)');
+  $('".dis_unit[data-state=\'' + state + '\']"').attr('fill', '#49929F');
+  $('".reg_unit[data-state=\'' + state + '\']"').attr('fill', '#D5F0F0');
 
 
   $('#stateName').text(state);
@@ -371,8 +371,8 @@ function setDropdown(width, height) {
 
 function getColor(status) {
   switch (status) {
-    case 'With a Disability': return { 'fill': 'rgba(255,0,0,0.6)', 'stroke': 'white' };
-    case 'No Disability': return { 'fill': 'rgba(255,0,0,0.1)', 'stroke': 'white' };
+    case 'With a Disability': return { 'fill': '#FFAC1D', 'stroke': 'white' };
+    case 'No Disability': return { 'fill': '#FFE5AE', 'stroke': 'white' };
   }
 }
 
